2e23708789381a83d2e0468f4a17330261273673
FelipeLevinir <apoyo.desarrollo3@congreso.cl>
2026-01-27 13:48:26 -0300

    implementación base del front, aun no está conectado al BFF

--- rick-and-morty-app/src/app/app.routes.ts
+++ rick-and-morty-app/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./shared/layout/shell/shell.component').then(m => m.ShellComponent),
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent),
+      },
+      {
+        path: 'episodes',
+        loadComponent: () =>
+          import('./features/episodes/episodes.component').then(m => m.EpisodesComponent),
+      },
+      {
+        path: 'characters',
+        loadComponent: () =>
+          import('./features/characters/characters.component').then(m => m.CharactersComponent),
+      },
+    ],
+  },
+  { path: '**', redirectTo: 'dashboard' },
+];
---
+++ rick-and-morty-app/src/app/features/characters/characters.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-characters',
+  templateUrl: './characters.component.html',
+  styleUrl: './characters.component.css',
+})
+export class CharactersComponent {}
